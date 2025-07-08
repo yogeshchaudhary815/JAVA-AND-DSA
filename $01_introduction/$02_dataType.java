84300034248850e4c3b7f25fa722dc2bccc886e6
yogesh chaudhary <yogeshchaudhary815>
2025-07-08 15:56:46 +0530

    check

--- $01_introduction/$02_dataType.java
+++ $01_introduction/$02_dataType.java
@@ -3,8 +3,8 @@ package $01_introduction;
 public class $02_dataType {
         public static void main(String[] args) {
         int number1 = 10;
-        byte number2 = 15 ;
-        short number3 =5 ;
+        byte number2 = 15;
+        short number3 =5;
         
         long longSum =1000*(number1+number2+number3);
         System.out.println(longSum);
